8a223670eb07379dc5b389acd6e14a72c6c2f88b
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-08-07 10:38:06 -0700

    Create (POST) a new action for a project

--- routers/actionsRouter.js
+++ routers/actionsRouter.js
@@ -2,6 +2,7 @@ const express = require("express");
 const projectsDB = require("../data/helpers/projectModel");
 const actionsDB = require("../data/helpers/actionModel");
 const validateProjectId = require("../middleware/validateProjectId");
+const validateAction = require("../middleware/validateAction");
 
 const router = express.Router();
 
@@ -13,7 +14,31 @@ router.get("/", validateProjectId, (req, res) => {
       res.status(200).json(actions);
     })
     .catch((error) => {
-      error: "There was an error getting the actions for all of your projects. Please try again. :)";
+      res.status(500).json({
+        error:
+          "There was an error getting the actions for all of your projects. Please try again. :)",
+      });
+    });
+});
+
+// Create (POST) a new action for a specific project
+router.post("/:id", validateProjectId, validateAction, (req, res) => {
+  const project = req.project;
+  const action = {
+    ...req.body,
+    project_id: project.id,
+  };
+
+  actionsDB
+    .insert(action)
+    .then((createdAction) => {
+      res.status(201).json(createdAction);
+    })
+    .catch((error) => {
+      res.status(500).json({
+        error:
+          "There was an error on our side creating your action. Please try again :)",
+      });
     });
 });
 
